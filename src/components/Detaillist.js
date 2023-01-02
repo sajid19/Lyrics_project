d328ba87a4a6237e90a1d5ba06e9edade01ee4ee
Foysal <foysal@iamsheba.xyz>
2023-01-02 19:04:50 +0600

    ll

--- src/components/Detaillist.js
+++ src/components/Detaillist.js
@@ -4,26 +4,28 @@ import Track from "./Track";
 import Search from "./Search";
 
 const Detaillist = (props) => {
-  // const [searchPoint, setSearchPoint] = useState("");
-  // const handleSearchData = (searchData) => {
-  //   setSearchPoint(searchData);
-  // };
-  //   console.log(props.datas);
-  //   const { track_list } = props.value;
-  //   console.log(track_list);
-  //   console.log(props);
-  // onSearchData={handleSearchData}
-  return (
-    <>
-      {/* <Search /> */}
-      <h2 className="heading">List of Music</h2>
-      <div className="row">
-        {props.datas.map((item) => {
-          return <Track key={item.track.track_id} tracks={item.track} />;
-        })}
-      </div>
-    </>
-  );
+	// const [searchPoint, setSearchPoint] = useState("");
+	// const handleSearchData = (searchData) => {
+	//   setSearchPoint(searchData);
+	// };
+	//   console.log(props.datas);
+	//   const { track_list } = props.value;
+	//   console.log(track_list);
+	//   console.log(props);
+	// onSearchData={handleSearchData}
+	return (
+		<>
+			{/* <Search /> */}
+			<h2 className="heading">List of Music</h2>
+			<div className="row">
+				{props.datas.map((item) => {
+					return (
+						<Track key={item.track.track_id} tracks={item.track} />
+					);
+				})}
+			</div>
+		</>
+	);
 };
 
 export default Detaillist;
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,52 +1,59 @@
 import React, { useEffect, useState } from "react";
 import Detaillist from "./Detaillist";
 import LoadingSpinner from "./Loadingspinner";
-import Search from "./Search";
-
+import "./Details.css";
 const Details = () => {
-  const [datas, setDatas] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [filteredData, setFilteredData] = useState(datas);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(
-          "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=35e9faf44615123fa08513d9a98e39c4"
-        );
-        const data = await res.json();
-        // console.log(data);
-        setDatas(data.message.body.track_list);
-        setFilteredData(data.message.body.track_list);
-        // console.log(data.message.body.track_list);
-        setLoading(false);
-        setError(null);
-      } catch (err) {
-        setError(error.message);
-      }
-    };
+	const [datas, setDatas] = useState([]);
+	const [isLoading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [searchSong, setSearchSong] = useState("justin");
+	// const [filteredData, setFilteredData] = useState(datas);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await fetch(
+					`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=${searchSong}&page_size=10&page=1&s_track_rating=desc&apikey=35e9faf44615123fa08513d9a98e39c4`
+				);
+				const data = await res.json();
+				setDatas(data.message.body.track_list);
+				setLoading(false);
+				setError(null);
+				// console.log(data.message.body.track_list);
+			} catch (err) {
+				setError(error);
+				// setError(error.message);
+			}
+		};
+		fetchData();
+	}, [searchSong]);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// console.log(e.target.name.value);
+		setSearchSong(e.target.name.value);
+	};
 
-    fetchData();
-  }, []);
-  // {datas && <Detaillist datas={datas} />}
-  const handleSearch = (searchValue) => {
-    let value = searchValue.toLowerCase();
-    const newTrack = datas.filter((item) => {
-      const artistName = item.track.artist_name.toLowerCase();
-      return artistName.startsWith(value);
-    });
-    setFilteredData(newTrack);
-  };
-  return (
-    <>
-      <Search onSearch={handleSearch} />
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        datas && <Detaillist datas={filteredData} />
-      )}
-    </>
-  );
+	return (
+		<>
+			<form onSubmit={handleSubmit} className="form">
+				<input placeholder="search Song" name="name" />
+				<button>search</button>
+			</form>
+
+			{isLoading ? (
+				<LoadingSpinner />
+			) : (
+				datas && <Detaillist datas={datas} />
+			)}
+		</>
+	);
 };
-
 export default Details;
+// {datas && <Detaillist datas={datas} />}
+// const handleSearch = (searchValue) => {
+//  let value = searchValue.toLowerCase();
+//  const newTrack = datas.filter((item) => {
+//    const artistName = item.track.artist_name.toLowerCase();
+//    return artistName.startsWith(value);
+//  });
+//  setFilteredData(newTrack);
+// };
